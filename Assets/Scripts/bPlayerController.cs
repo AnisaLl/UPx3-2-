2958bc172ea0863d004738ce2630e43da00ef718
Annie-Wang <zhouyulai92>
2015-11-13 10:19:27 +0100

    first commit

---
+++ Assets/Scripts/bPlayerController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bPlayerController : MonoBehaviour {
+	
+	public float speed;
+	public float jumpHeight;
+	
+	private Rigidbody rb;
+	private bool isFalling = false;
+	
+	
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	void FixedUpdate ()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+
+		Vector3 movement = new Vector3 (0.0f , 0.0f, moveHorizontal);
+		//speed *= Time.deltaTime;
+		rb.AddForce (movement * speed);
+
+		
+		if (Input.GetButtonDown("Jump") && isFalling == false)
+		{
+			rb.velocity = new Vector2(this.rb.velocity.x, jumpHeight);
+//		    rb.velocity = new Vector3(0, jumpHeight, 0);
+//			Vector3 v = rb.velocity;
+//			v.y = jumpHeight;
+		}
+		isFalling = true;
+	}
+	
+	void OnCollisionStay ()
+	{
+		isFalling = false;
+	}
+}
+
