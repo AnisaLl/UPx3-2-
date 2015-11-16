4837176110d988a4d9527e94af33108cb9932796
Annie-Wang <zhouyulai92>
2015-11-16 01:48:26 +0100

    Camera rotates perfectly now

--- Assets/Scripts/bPlayerController.cs
+++ Assets/Scripts/bPlayerController.cs
@@ -5,38 +5,75 @@ public class bPlayerController : MonoBehaviour {
 	
 	public float speed;
 	public float jumpHeight;
-	
-	private Rigidbody rb;
+
+    public float gravity = .1f;
+    public float friction = .01f;
+
+    private Vector3 _velocity;
+    private Vector3 _pos;
+//    private CharacterController _charController;
+
+    private Rigidbody rb;
 	private bool isFalling = false;
 	
 	
 	void Start ()
 	{
 		rb = GetComponent<Rigidbody>();
-	}
-	
-	void FixedUpdate ()
-	{
-		float moveHorizontal = Input.GetAxis ("Horizontal");
+        _velocity = new Vector3();
+        _pos = gameObject.transform.position;
+//        _charController = GetComponentInChildren<CharacterController>();
+    }
 
-		Vector3 movement = new Vector3 (0.0f , 0.0f, moveHorizontal);
-		//speed *= Time.deltaTime;
-		rb.AddForce (movement * speed);
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
 
-		
-		if (Input.GetButtonDown("Jump") && isFalling == false)
-		{
-			rb.velocity = new Vector2(this.rb.velocity.x, jumpHeight);
-//		    rb.velocity = new Vector3(0, jumpHeight, 0);
-//			Vector3 v = rb.velocity;
-//			v.y = jumpHeight;
-		}
-		isFalling = true;
-	}
-	
-	void OnCollisionStay ()
-	{
-		isFalling = false;
-	}
+        Vector3 movement = new Vector3(0.0f, 0.0f, moveHorizontal);
+        //speed *= Time.deltaTime;
+        rb.AddForce(movement * speed);
+
+
+        if (Input.GetButtonDown("Jump") && isFalling == false)
+        {
+            rb.velocity = new Vector2(this.rb.velocity.x, jumpHeight);
+            //		    rb.velocity = new Vector3(0, jumpHeight, 0);
+            //			Vector3 v = rb.velocity;
+            //			v.y = jumpHeight;
+        }
+        isFalling = true;
+    }
+
+    void OnCollisionStay()
+    {
+        isFalling = false;
+    }
+
+    //void Update()
+    //{
+    //    float hInput = Input.GetAxis("Horizontal");
+    //    _velocity.x += (hInput * speed);
+
+    //    _velocity.x *= friction;
+
+    //    Debug.Log(_velocity);
+
+    //    Debug.Log(_charController.isGrounded);
+
+    //    if (!_charController.isGrounded)
+    //    {
+    //        _velocity.y -= gravity;
+    //    }
+
+    //    if (Input.GetButtonDown("Jump"))
+    //    {
+    //        Debug.Log("jump");
+    //        _velocity.y = jumpHeight;
+    //    }
+
+    //    _pos += (_velocity * Time.deltaTime);
+
+    //    _charController.transform.position = _pos;
+    //}
 }
 
---
+++ Assets/Scripts/prismaController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class prismaController : MonoBehaviour {
+
+    private GameObject crystal;
+
+	// Use this for initialization
+	void Start () {
+        crystal = GameObject.FindGameObjectWithTag("crystal");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        lookAtCamera();
+    }
+
+    void lookAtCamera()
+    {
+
+    }
+}
