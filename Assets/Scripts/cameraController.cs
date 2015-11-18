56df626d94616dac057cc128a02cde7a7342e6a5
Anisa Llaveshi <llaveshianisa@gmail.com>
2015-11-18 22:34:45 +0100

    Switching between the three characters

--- Assets/Scripts/cameraController.cs
+++ Assets/Scripts/cameraController.cs
@@ -12,6 +12,9 @@ public class cameraController : MonoBehaviour {
     private GameObject player1;
     private GameObject player2;
     private GameObject player3;
+    private float worldCenterY;
+
+    private bool canCallFunction;
 
     // Use this for initialization
     void Start()
@@ -25,57 +28,47 @@ public class cameraController : MonoBehaviour {
         player1.GetComponent<Char1Controller>().enabled = false;
         player2.GetComponent<Char2Controller>().enabled = false;
         player3.GetComponent<Char3Controller>().enabled = true;
+        worldCenterY = this.GetComponent<Transform>().eulerAngles.y;
+        //worldCenterY = GameObject.FindGameObjectWithTag("worldCenter").transform.rotation.y;
     }
 
     // Update is called once per frame
     void Update()
     {
+        //worldCenterY = GameObject.FindGameObjectWithTag("worldCenter").transform.rotation.y;
         if (Input.GetKeyDown(KeyCode.Z))
         {
             rotateTween(120);
-            if (GameObject.Find("worldCenter").transform.rotation.y > -20 && GameObject.Find("worldCenter").transform.rotation.y < 20)
-            {
-                player1.GetComponent<Char1Controller>().enabled = false;
-                player2.GetComponent<Char2Controller>().enabled = false;
-                player3.GetComponent<Char3Controller>().enabled = true;
-            }
-            if (GameObject.Find("worldCenter").transform.rotation.y > 100 && GameObject.Find("worldCenter").transform.rotation.y < 140)
-            {
-                player1.GetComponent<Char1Controller>().enabled = false;
-                player2.GetComponent<Char2Controller>().enabled = true;
-                player3.GetComponent<Char3Controller>().enabled = false;
-            }
-            if (GameObject.Find("worldCenter").transform.rotation.y > 220 && GameObject.Find("worldCenter").transform.rotation.y < 260)
-            {
-                player1.GetComponent<Char1Controller>().enabled = true;
-                player2.GetComponent<Char2Controller>().enabled = false;
-                player3.GetComponent<Char3Controller>().enabled = false;
-            }
+            StartCoroutine(MyCoroutine());
+            
         }
 
         if (Input.GetKeyDown(KeyCode.X))
         {
             rotateTween(-120);
-            if (GameObject.Find("worldCenter").transform.rotation.y > -20 && GameObject.Find("worldCenter").transform.rotation.y < 20)
-            {
-                player1.GetComponent<Char1Controller>().enabled = false;
-                player2.GetComponent<Char2Controller>().enabled = false;
-                player3.GetComponent<Char3Controller>().enabled = true;
-            }
-            if (GameObject.Find("worldCenter").transform.rotation.y > 100 && GameObject.Find("worldCenter").transform.rotation.y < 140)
-            {
-                player1.GetComponent<Char1Controller>().enabled = false;
-                player2.GetComponent<Char2Controller>().enabled = true;
-                player3.GetComponent<Char3Controller>().enabled = false;
-            }
-            if (GameObject.Find("worldCenter").transform.rotation.y > 220 && GameObject.Find("worldCenter").transform.rotation.y < 260)
-            {
-                player1.GetComponent<Char1Controller>().enabled = true;
-                player2.GetComponent<Char2Controller>().enabled = false;
-                player3.GetComponent<Char3Controller>().enabled = false;
-            }
+            StartCoroutine(MyCoroutine());
+            //worldCenterY = this.GetComponent<Transform>().eulerAngles.y;
+            ////worldCenterY = GameObject.FindGameObjectWithTag("worldCenter").transform.rotation.y;
+            //Debug.Log(worldCenterY);
+            //if (worldCenterY > -20.0f && worldCenterY < 20.0f)
+            //{
+            //    player1.GetComponent<Char1Controller>().enabled = false;
+            //    player2.GetComponent<Char2Controller>().enabled = true;
+            //    player3.GetComponent<Char3Controller>().enabled = false;
+            //}
+            //else if (worldCenterY > 100.0f && worldCenterY < 140.0f)
+            //{
+            //    player1.GetComponent<Char1Controller>().enabled = true;
+            //    player2.GetComponent<Char2Controller>().enabled = false;
+            //    player3.GetComponent<Char3Controller>().enabled = false;
+            //}
+            //else if (worldCenterY > 220.0f && worldCenterY < 260.0f)
+            //{
+            //    player1.GetComponent<Char1Controller>().enabled = true;
+            //    player2.GetComponent<Char2Controller>().enabled = false;
+            //    player3.GetComponent<Char3Controller>().enabled = false;
+            //}
         }
-
     }
 
     private void rotateTween(float amount)
@@ -93,5 +86,37 @@ public class cameraController : MonoBehaviour {
     {
         _isTweening = false;
         //_disablePlayer.enable();
+    }
+
+    IEnumerator MyCoroutine()
+    {
+        //This is a coroutine
+        //Debug.Log("time start");
+
+        yield return new WaitForSeconds(1);
+
+        worldCenterY = this.GetComponent<Transform>().eulerAngles.y;
+        //worldCenterY = GameObject.FindGameObjectWithTag("worldCenter").transform.rotation.y;
+        Debug.Log(worldCenterY);
+        if (worldCenterY > -20.0f && worldCenterY < 20.0f)
+        {
+            player1.GetComponent<Char1Controller>().enabled = false;
+            player2.GetComponent<Char2Controller>().enabled = false;
+            player3.GetComponent<Char3Controller>().enabled = true;
+        }
+        else if (worldCenterY > 100.0f && worldCenterY < 140.0f)
+        {
+            player1.GetComponent<Char1Controller>().enabled = false;
+            player2.GetComponent<Char2Controller>().enabled = true;
+            player3.GetComponent<Char3Controller>().enabled = false;
+        }
+        else if (worldCenterY > 220.0f && worldCenterY < 260.0f)
+        {
+            player1.GetComponent<Char1Controller>().enabled = true;
+            player2.GetComponent<Char2Controller>().enabled = false;
+            player3.GetComponent<Char3Controller>().enabled = false;
+        }
+        yield return new WaitForSeconds(1);
+        // Debug.Log("time end");
     }
 }
