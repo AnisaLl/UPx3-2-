cbabdc2608e5c8e3a5337a3c8de758a42394f9e1
Annie-Wang <zhouyulai92>
2015-11-30 03:28:59 +0100

    audio, game over mechanics, add camera effects

--- Assets/Scripts/triggerController.cs
+++ Assets/Scripts/triggerController.cs
@@ -3,29 +3,30 @@ using System.Collections;
 
 public class triggerController : MonoBehaviour {
 
-    float timer = 0.0f;
+
     public float range = 3f;
     Ray shootRay;                                   // A ray from the trigger forwards.
     ParticleSystem triggerParticles;                    // Reference to the particle system.
-    LineRenderer triggerLine;                           // Reference to the line renderer.
     AudioSource triggerAudio;                           // Reference to the audio source.
     Light triggerLight;                                 // Reference to the light component.
     public float effectsDisplayTime = 2.0f;
     private GameObject trigger;
+    GameObject camera2;
 
     // Use this for initialization
     void Awake()
     {
         // Set up the references.
         triggerParticles = GetComponent<ParticleSystem>();
-        triggerLine = GetComponent<LineRenderer>();
-        //triggerAudio = GetComponent<AudioSource>();
+        triggerAudio = GetComponent<AudioSource>();
         triggerLight = GetComponent<Light>();
     }
 
     void Start () {
         trigger = GameObject.FindGameObjectWithTag("trigger_f1_1");
         gameObject.transform.position = new Vector3(trigger.transform.position.x, trigger.transform.position.y, trigger.transform.position.z);
+        camera2 = GameObject.FindGameObjectWithTag("camera2");
+        camera2.SetActive(false); 
     }
 	
 	// Update is called once per frame
@@ -51,13 +52,16 @@ public class triggerController : MonoBehaviour {
         // Disable the line renderer and the light.
         //triggerLine.enabled = false;
         triggerLight.enabled = false;
+        camera2.SetActive(false);
+        triggerParticles.Stop();
     }
 
     public void Display()
     {
+        trigger.SetActive(false);
 
         // Play the trigger audioclip.
-        //triggerAudio.Play();
+        triggerAudio.Play();
 
         // Enable the light.
         triggerLight.enabled = true;
@@ -65,15 +69,13 @@ public class triggerController : MonoBehaviour {
         //  start the particles.
         triggerParticles.Play();
 
-        // Enable the line renderer and set it's first position to be the position of the trigger.
-        triggerLine.enabled = true;
-        triggerLine.SetPosition(0, transform.position);
-
         // Set the shootRay so that it starts at the position of the trigger and points forward.
         shootRay.origin = transform.position;
         shootRay.direction = transform.forward;
-       
-        triggerLine.SetPosition(1, shootRay.origin + shootRay.direction * range);
-     
+
+        camera2.SetActive(true);
+
+        Invoke("DisableEffects", 4.0f);
+
     }
 }
