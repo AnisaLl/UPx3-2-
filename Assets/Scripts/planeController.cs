0e0b709757b33855fd2c105940b7ac8613b6935a
Annie-Wang <zhouyulai92>
2015-12-02 02:09:14 +0100

    camera changes

---
+++ Assets/Scripts/planeController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class planeController : MonoBehaviour {
+    // camera speed
+    public float cameraSpeed = 0.8f;
+
+    private Vector3 tCamerPos;
+    // trigger altitude
+    public float tTriggerAltitude = 30.0f;
+
+    //game over controll
+    private GameOver Isgameover;
+    // Use this for initialization
+    void Start () {
+        Isgameover = GameObject.FindGameObjectWithTag("plane").GetComponent<GameOver>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        // camera's local position
+        tCamerPos = gameObject.transform.localPosition;
+
+        if (tCamerPos.y < tTriggerAltitude && !Isgameover.IsGameOver())
+        {
+            gameObject.transform.Translate(0, Time.deltaTime * cameraSpeed, 0);
+        }
+
+    }
+}
