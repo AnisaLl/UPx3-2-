68674f0678f5b18eee49581eec1f249931620023
Annie-Wang <zhouyulai92>
2015-12-07 22:05:32 +0100

    update for scene

--- Assets/Scripts/startMenuController.cs
+++ Assets/Scripts/startMenuController.cs
@@ -14,9 +14,11 @@ public class startMenuController : MonoBehaviour {
 	
 	}
 
-    public void showPanel_option(GameObject panel)
+    public void showPanel_option(GameObject panel_option)
     {     
-        panel.SetActive(!panel.activeSelf);
+        panel_option.SetActive(!panel_option.activeSelf);
+        //panel_main.SetActive(!panel_main.activeSelf);
+
     }
 
     public void LoadScene(int level)
